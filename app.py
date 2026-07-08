2bcb84aa6070d665f62b1dcf3aca340ec43375f8
Ayush Kumar Tiwari <ayushtiwari972005@gmail.com>
2026-07-08 15:45:23 +0530

    Modify developer info and change page icon
    Updated developer information and page icon.

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ from PIL import Image
 # ---------------------------------------------------
 st.set_page_config(
     page_title="COVID-19 Chest X-Ray Detection",
-    page_icon="🩺",
+    page_icon="🫀",
     layout="wide"
 )
 
@@ -24,7 +24,7 @@ model = load_model()
 # ---------------------------------------------------
 # Title
 # ---------------------------------------------------
-st.title("🩺 COVID-19 Detection from Chest X-Ray")
+st.title("🫀 COVID-19 Detection from Chest X-Ray")
 
 st.write(
     """
@@ -80,12 +80,8 @@ if uploaded_file is not None:
 # Developer Corner
 # ---------------------------------------------------
 st.markdown("---")
-st.header("👨‍💻 Developer Corner")
+st.header("💻 Developer Corner")
 
 st.markdown("""
-**Developer:** Vivek Srivastava
+**Developer:** Ayush Kumar Tiwari
 
-🔗 **LinkedIn:** www.linkedin.com/in/vivek-srivastava-0a878a329
-
-💻 **GitHub:** https://github.com/viveksrivastava045-cyber/covid_19_detection
-""")
